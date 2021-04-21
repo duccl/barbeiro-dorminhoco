5ea5237a695fb3fab65253c5bb511dc3761205c7
duccl <edr.ccol@gmail.com>
2021-04-20 21:25:29 -0300

    Tempo para cortar o cabelo adc no cliente

--- src/Cliente.cs
+++ src/Cliente.cs
@@ -1,8 +1,12 @@
+
+using System;
+
 namespace barbeiro_dorminhoco
 {
     public class Cliente
     {
         public string Name { get; set; }
-        public decimal TempoAguardando { get; set; }
+        public double TempoAguardando { get; set; }
+        public int TempoParaCortarCabelo => new Random(3).Next(0,4000);
     }
 }
